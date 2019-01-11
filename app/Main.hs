f41dd5c5361ca14b93816bfaacc95291bd7ef9d7
guyslain.naves <guyslain.naves@lis-lab.fr>
2019-01-11 13:50:58 +0100

    Update Main

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,15 @@ import qualified Day2part1
 import qualified Day2part2
 import qualified Day3
 import qualified Day4
+import qualified Day5
+import qualified Day6
+import qualified Day7
+import qualified Day8
+import qualified Day9
+import qualified Day10
+import qualified Day11
+import qualified Day12
+import qualified Day13
 
 chooseProcess :: String -> String -> String
 chooseProcess arg =
@@ -19,9 +28,26 @@ chooseProcess arg =
     "day3part1" -> Day3.processPart1
     "day3part2" -> Day3.processPart2
     "day4part1" -> Day4.processPart1
+    "day4part2" -> Day4.processPart2
+    "day5part1" -> Day5.processPart1
+    "day5part2" -> Day5.processPart2
+    "day6part1" -> Day6.processPart1
+    "day6part2" -> Day6.processPart2
+    "day7part1" -> Day7.processPart1
+    "day7part2" -> Day7.processPart2
+    "day8part1" -> Day8.processPart1
+    "day8part2" -> Day8.processPart2
+    "day9part1" -> Day9.processPart1
+    "day10part1" -> Day10.processPart1
+    "day11part1" -> Day11.processPart1
+    "day11part2" -> Day11.processPart2
+    "day12part1" -> Day12.processPart1
+    "day12part2" -> Day12.processPart2
+    "day13part1" -> Day13.processPart1
+    "day13part2" -> Day13.processPart2
     
 main :: IO ()
 main = do
   input <- getContents
   [arg] <- getArgs
-  print (chooseProcess arg input)
+  putStrLn (chooseProcess arg input)
--- src/Day4.hs
+++ src/Day4.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Day4 (processPart1) where
+module Day4 (processPart1, processPart2) where
 
-import Text.Trifecta
+import Text.Trifecta hiding (count)
 import Text.Parser.Token
 import qualified Data.Text as T
 
 import Data.Time.Clock
 import Data.Time.Calendar
 import Data.Time.LocalTime
+import Data.List (sortBy, groupBy, foldl, maximumBy)
+import Data.Array.IArray
+import GHC.Exts (sortWith)
 
 import Control.Applicative
 
@@ -24,9 +27,22 @@ data EventInfo =
   deriving Show
 
 data Event =
-  Event UTCTime EventInfo
+  Event { time :: UTCTime, info :: EventInfo }
   deriving Show
 
+data Night =
+  Night { guard :: Int, events :: [Event] }
+  deriving Show
+
+data GuardStats =
+  Stats
+    { guardId :: Int,
+      minutesSlept :: Int,
+      timetable :: Array Int Int 
+    }
+
+dummyGuard :: GuardStats
+dummyGuard = Stats (-1) 0 (array (0, 59) [ (i,0) | i <- [0..59] ])
 
 infoParser :: Parser EventInfo
 infoParser =
@@ -59,7 +75,7 @@ eventParser = do
   _ <- symbolic ']'
   info <- infoParser
   return $
-    Event
+    Event 
       (UTCTime (fromGregorian year month day) (toDiffTime hour minute))
       info
   where int = fmap fromInteger integer
@@ -70,8 +86,120 @@ parseEvent line =
   case parseString eventParser mempty line of
     Success event -> event
 
-processPart1 :: String -> String
-processPart1 =
-  show
+
+partitionAt :: (a -> Bool) -> [a] -> [[a]]
+partitionAt condition = go [] []
+  where
+    go groups group [] = reverse (reverse group : groups)
+    go groups group (x:xs)
+      | condition x && null group = go groups [x] xs
+      | condition x = go (reverse group : groups) [x] xs
+      | otherwise = go groups (x:group) xs
+
+
+
+annotateByGuard events@(Event _ (Shift guard) : _) =
+  Night guard events
+
+  
+        
+-- groupEventsByGuard :: [Event] -> [[Night]]
+groupEventsByGuard =
+    groupBy sameGuard
+  . sortWith guard
+  . map annotateByGuard
+  . partitionAt (isShift . info)
+  . sortWith time
+  where
+    isShift (Shift _) = True
+    isShift _ = False
+    sameGuard night1 night2 = guard night1 == guard night2
+
+minuteOfTime :: UTCTime -> Int
+minuteOfTime =
+  todMin
+  . localTimeOfDay
+  . utcToLocalTime utc
+
+
+recordNightStats :: GuardStats -> Night -> GuardStats
+recordNightStats guard =
+  fst . foldl recordEvent (guard, 0) . events
+    where
+      recordEvent (guard, nextMinute) (Event time (Shift gid)) =
+        (Stats gid (minutesSlept guard) (timetable guard), nextMinute)
+      recordEvent (guard, nextMinute) (Event time FallsAsleep) =
+        (guard, minuteOfTime time)
+      recordEvent (Stats gid minSlept table, nextMinute) (Event time WakesUp) =
+        (Stats
+          gid
+          (minSlept + minuteNow - nextMinute)
+          (table // [(i, table ! i + 1) | i <- [nextMinute .. minuteNow - 1]]),
+         minuteNow
+        )
+        where minuteNow = minuteOfTime time 
+
+recordAllNights :: [ Night ] -> GuardStats
+recordAllNights =
+  foldl recordNightStats dummyGuard
+
+compileGuardsStats :: [[Night]] -> [ GuardStats ]
+compileGuardsStats = map recordAllNights
+
+
+data Candidate =
+  Candidate
+  { guardStats :: GuardStats
+  , minute :: Int
+  , count :: Int
+  }
+    
+    
+
+maxSleptMinute :: GuardStats -> Candidate
+maxSleptMinute stats@(Stats gid _ table) =
+  Candidate stats index max
+  where
+    (index, max) =
+      head
+      . reverse
+      . sortBy (\(i1,c1) (i2, c2) -> compare c1 c2)
+      $ assocs table
+
+strategy1 :: [GuardStats] -> Candidate
+strategy1 =
+  maxSleptMinute
+  . maximumBy (\st1 st2 -> compare (minutesSlept st1) (minutesSlept st2))
+
+strategy2 :: [GuardStats] -> Candidate
+strategy2 =
+  maximumBy (\c1 c2 -> compare (count c1) (count c2))
+  . map maxSleptMinute
+
+
+preProcess :: String -> [GuardStats]
+preProcess =
+    compileGuardsStats
+  . groupEventsByGuard
   . map parseEvent
   . lines
+
+
+postProcess :: Candidate -> String
+postProcess =
+    show
+  . (\ cand -> (guardId . guardStats $ cand) * (minute cand))
+
+processPart1 :: String -> String
+processPart1 =
+  postProcess
+  . strategy1
+  . preProcess
+
+
+processPart2 :: String -> String
+processPart2 = 
+  postProcess
+  . strategy2
+  . preProcess
+  
